73cd25e1eeffe1bb3033290f8ab747160723a595
PRAVEEN KUMAR KOTEKAL <praveenkotekal@gmail.com>
2016-09-10 03:28:33 -0500

    Add files via upload

---
+++ edmund_helper.py
@@ -0,0 +1,213 @@
+##################################################################################
+##										     			      ##
+##  Praveen Kumar Kotekal									  ##
+##  Paper - Edmunds API Customer review and ratings Data Pull ##
+##             												  ##
+##														      ##
+##################################################################################
+
+# import necessary python libraries
+from urllib2 import Request, build_opener
+import simplejson
+import pandas as pd
+#import json
+
+# edmunds.com API Key
+key = 'sc3svg35bkwdr2nr8qg46prf'
+fmt_api_key = 'fmt=json&api_key=' + key
+
+#used to make omdbapi.com think we are getting this data from a browser
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = { 'User-Agent' : user_agent }
+
+#scope of the api calls
+parent_companies = ['honda', 'ford', 'toyota', 'fiat chrysler', 'general motors']
+makes = ['acura','buick','cadillac','chevrolet','chrysler','dodge','fiat','ford','gmc','honda','jeep','lexus','lincoln','maserati','ram','scion','toyota']
+years = ['2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016']
+
+# base url for Edmunds Vehicle API
+base_api_url = 'https://api.edmunds.com/api/vehicle/v2/'
+
+list_json = []
+
+for make in makes:
+    for year in years:
+        try:        
+            url = base_api_url + make + '/models?year=' + year + '&' + fmt_api_key
+            req = Request(url, None, headers) #api request
+            opener = build_opener() #submit the api request
+            the_page = opener.open(req) #read the json from the api query
+            json = simplejson.load(the_page) #convert the json to a dict
+            json['make'] = make           
+            list_json.append(json)
+        except:
+            continue
+
+# store models json into a new dictionary object
+data = {'items':list_json}
+
+# initiate a new list to store the model year IDs
+model_list = []
+
+# loop through each object in the data dictionary to store the make, model, year ID, and year
+for item in data['items']:
+    for model in item['models']:
+        for year in model['years']:
+            item_data = [item['make'], model['niceName'], str(year['id']), year['year']]
+            model_list.append(item_data)
+            
+# store the model list into a dataframe
+model_years_df = pd.DataFrame(model_list, columns=['make', 'model', 'year_id', 'year'])
+
+# initialize a list to store the recall information
+recalls = []
+
+# API call for recalls using model year IDs
+for index, row in model_years_df.iterrows():
+    try:    
+        year_id = row['year_id']
+        url = 'https://api.edmunds.com/v1/api/maintenance/recallrepository/findbymodelyearid?modelyearid=' + year_id + '&' + fmt_api_key
+        req = Request(url, None, headers) #api request
+        opener = build_opener() #submit the api request
+        the_page = opener.open(req) #read the json from the api query
+        json = simplejson.load(the_page) #convert the json to a dict
+        json['make'] = row['make']
+        json['model'] = row['model']
+        json['year_id'] = row['year_id']
+        json['year'] = row['year']
+        recalls.append(json)
+    except:
+        continue
+
+# initialize another recall list to store details in tabular format
+recall_list = []    
+    
+for item in recalls:
+    for recall in item['recallHolder']:
+        data = [item['make'], item['model'], str(item['year']), item['year_id'], recall['componentDescription'], recall['defectConsequence'], 
+                recall['defectCorrectiveAction'], recall['defectDescription'], recall['numberOfVehiclesAffected'], recall['influencedBy'],
+                recall['recallNumber']]
+        recall_list.append(data)
+        
+# save the list of recalls to a dataframe
+recall_df = pd.DataFrame(recall_list, columns=['make', 'model', 'year', 'year_id', 'componentDescription', 'defectConsequence',
+                                               'defectCorrectiveAction', 'defectDescription', 'numberOfVehiclesAffected',
+                                               'influencedBy', 'recallNumber'])
+
+# save the recall data to an excel file
+recall_df.to_excel('recalls.xlsx', encoding='utf-8', index=False)
+
+##############################################################
+##
+##  Edmunds Editorial API - Articles
+##
+##############################################################
+
+# read recall data in from Excel
+makes = pd.read_excel('recalls.xlsx')
+
+# editorial_url base
+editorial_url = 'https://api.edmunds.com/api/editorial/v2/'
+
+articles = []
+
+# loop through and get all articles related to each make/model/year
+for index, row in makes.iterrows():
+    try:    
+        make = row['make']
+        model = row['model']
+        year = str(row['year'])
+        url = editorial_url + make + '/' + model + '/' + year + '?view=basic&' + fmt_api_key
+        req = Request(url, None, headers) #api request
+        opener = build_opener() #submit the api request
+        the_page = opener.open(req) #read the json from the api query
+        json = simplejson.load(the_page) #convert the json to a dict
+        articles.append(json)
+    except:
+        continue
+    
+# flattened json list
+art_list = []
+
+# loop through articles and add them into a list
+for article in articles:
+    
+    try:    
+        cons = article['cons'][0]
+    except:
+        cons = ''
+    
+    try:
+        edmundsSays = article['edmundsSays']
+    except:
+        edmundsSays = ''        
+
+    try:
+        make = article['make']['niceName']
+    except:       
+        make = ''  
+        
+    try:
+        model = article['model']['niceName']
+    except:
+        model = ''
+
+    try:
+        pros = article['pros'][0]
+    except:
+        pros = ''
+        
+    try:        
+        title = article['title']
+    except:
+        title = ''        
+        
+    try:
+        whatsNew = article['whatsNew']
+    except:
+        whatsNew = ''        
+        
+    try:
+        year = article['year']['year']
+    except:
+        year = ''        
+        
+    try:
+        body = article['body']
+    except:
+        body = ''       
+        
+    try:
+        intro = article['introduction']
+    except:
+        intro = ''       
+        
+    try:
+        driving = article['driving']
+    except:
+        driving = ''
+        
+    try:
+        interior = article['interior']
+    except:
+        interior = ''    
+        
+    try:
+        powertrain = article['powertrain']
+    except:
+        powertrain = ''   
+
+    try:
+        safety = article['safety']
+    except:
+        safety = ''
+    
+    art = [make, model, year, pros, cons, title, body, intro, edmundsSays, whatsNew, driving, interior, powertrain, safety]
+    art_list.append(art)
+
+# save articles to a dataframe    
+articles_df = pd.DataFrame(art_list, columns=['make', 'model', 'year', 'pros', 'cons', 'title', 'body', 'intro', 'edmundsSays', 
+                                              'whatsNew', 'driving', 'interior', 'powertrain', 'safety'])
+
+# save articles to Excel
+articles_df.to_excel('articles.xlsx', index=False, encoding='utf-8')
---
+++ login.py
@@ -0,0 +1,288 @@
+
+from urllib2 import Request, build_opener
+import simplejson
+import pandas as pd
+#import json
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = { 'User-Agent' : user_agent }
+
+# edmunds.com API Key
+key = 'sc3svg35bkwdr2nr8qg46prf'
+fmt_api_key = 'fmt=json&api_key=' + key
+#  API call for recalls using model year IDs
+# for index, row in model_years_df.iterrows():
+years = ['2014']
+models = ['impreza-wrx',]
+
+
+# url1 = 'https://api.edmunds.com/v1/api/maintenance/recallrepository/findbymodelyearid?modelyearid=' + year_id + '&' + fmt_api_key
+
+records = []
+for model in models:
+    for year in years:
+
+        try:
+        # url = 'https://api.edmunds.com/api/vehiclereviews/v2/honda/accord/2015?' + fmt_api_key
+            u = 'https://api.edmunds.com/api/vehiclereviews/v2/subaru/' + model + '/' + year + '?' + fmt_api_key
+            print u
+            req = Request(u, None, headers) #api request
+            opener = build_opener() #submit the api request
+            the_page = opener.open(req) #read the json from the api query
+            json = simplejson.load(the_page) #convert the json to a dict
+            for review in json['reviews']:
+
+
+                try:
+                    text_review = review['text']
+                except:
+                    text_review = ''
+
+                try:
+                    userRating = review['userRating']
+                except:
+                    userRating = ''
+
+                try:
+                    styleId = review['styleId']
+                except:
+                    styleId = ''
+
+                try:
+                    id = review['id']
+                except:
+                    id = ''
+
+                try:
+                    purchaseDate = review['carDetailsDto']['purchaseDate']
+                except:
+                    purchaseDate = ''
+
+                try:
+                    pricePaid = review['carDetailsDto']['pricePaid']
+                except:
+                    pricePaid = ''
+
+                try:
+                    odometerMiles = review['carDetailsDto']['odometerMiles']
+                except:
+                    odometerMiles = ''
+
+                try:
+                    title = review['title']
+                except:
+                    title = ''
+
+                try:
+                    commentsCount = review['commentsCount']
+                except:
+                    commentsCount = ''
+
+                try:
+                    averageRating = review['averageRating']
+                except:
+                    averageRating = ''
+
+                try:
+                    rearSeats = review['comfortRatingDto']['rearSeats']
+                except:
+                    rearSeats = ''
+
+                try:
+                    frontSeats = review['comfortRatingDto']['frontSeats']
+                except:
+                    frontSeats = ''
+
+
+                try:
+                    comfortRating = review['comfortRatingDto']['comfortRating']
+                except:
+                    comfortRating = ''
+
+
+                try:
+                    gettingInOut = review['comfortRatingDto']['gettingInOut']
+                except:
+                    gettingInOut = ''
+
+                try:
+                    rideComfort = review['comfortRatingDto']['rideComfort']
+                except:
+                    rideComfort = ''
+
+
+                try:
+                    noiseAndVibration = review['comfortRatingDto']['noiseAndVibration']
+                except:
+                    noiseAndVibration = ''
+
+                try:
+                    authorName = review['author']['authorName']
+                except:
+                    authorName = ''
+
+                try:
+                    entertainment = review['technologyRatingDto']['entertainment']
+                except:
+                    entertainment = ''
+
+                try:
+                    climateControl = review['technologyRatingDto']['climateControl']
+                except:
+                    climateControl = ''
+
+                try:
+                    bluetooth = review['technologyRatingDto']['bluetooth']
+                except:
+                    bluetooth = ''
+
+                try:
+                    usbPorts = review['technologyRatingDto']['usbPorts']
+                except:
+                    usbPorts = ''
+
+                try:
+                    technologyRating = review['technologyRatingDto']['technologyRating']
+                except:
+                    technologyRating = ''
+
+                try:
+                    navigation = review['technologyRatingDto']['navigation']
+                except:
+                    navigation = ''
+
+                try:
+                    parkingAids = review['safetyRatingDto']['parkingAids']
+                except:
+                    parkingAids = ''
+
+
+                try:
+                    outwardVisibility = review['safetyRatingDto']['outwardVisibility']
+                except:
+                    outwardVisibility = ''
+
+
+                try:
+                    rainSnowTraction = review['safetyRatingDto']['rainSnowTraction']
+                except:
+                    rainSnowTraction = ''
+
+
+                try:
+                    safetyRating = review['safetyRatingDto']['safetyRating']
+                except:
+                    safetyRating = ''
+
+                try:
+                    activeSafety = review['safetyRatingDto']['activeSafety']
+                except:
+                    activeSafety = ''
+
+
+                try:
+                    headlights = review['safetyRatingDto']['headlights']
+                except:
+                    headlights = ''
+
+
+                try:
+                    safetyRating = review['safetyRatingDto']['safetyRating']
+                except:
+                    safetyRating = ''
+
+                try:
+                    instrumentation = review['interiorRatingDto']['instrumentation']
+                except:
+                    instrumentation = ''
+
+
+                try:
+                    interiorDesign = review['interiorRatingDto']['interiorDesign']
+                except:
+                    interiorDesign = ''
+
+                try:
+                    qualityOfMaterials = review['interiorRatingDto']['qualityOfMaterials']
+                except:
+                    qualityOfMaterials = ''
+
+                try:
+                    logicOfControls = review['interiorRatingDto']['logicOfControls']
+                except:
+                    logicOfControls = ''
+
+                try:
+                    interiorRating = review['interiorRatingDto']['interiorRating']
+                except:
+                    interiorRating = ''
+
+                try:
+                    cargoStorage = review['interiorRatingDto']['cargoStorage']
+                except:
+                    cargoStorage = ''
+
+                try:
+                    acceleration = review['performanceRatingDto']['acceleration']
+                except:
+                    acceleration = ''
+
+                try:
+                    roadHolding = review['performanceRatingDto']['roadHolding']
+                except:
+                    roadHolding = ''
+
+                try:
+                    braking = review['performanceRatingDto']['braking']
+                except:
+                    braking = ''
+
+                try:
+                    performanceRating = review['performanceRatingDto']['performanceRating']
+                except:
+                    performanceRating = ''
+
+                try:
+                    shifting = review['performanceRatingDto']['shifting']
+                except:
+                    shifting = ''
+
+                try:
+                    steering = review['performanceRatingDto']['steering']
+                except:
+                    steering = ''
+
+
+                new_record = ['subaru',year, model, text_review, userRating, styleId, id, purchaseDate, pricePaid, odometerMiles, title, commentsCount, averageRating,  rearSeats, frontSeats, comfortRating, gettingInOut,
+                       rideComfort, noiseAndVibration, authorName, climateControl, bluetooth, usbPorts, technologyRating, navigation, parkingAids, outwardVisibility, rainSnowTraction, safetyRating,
+                       activeSafety, headlights, safetyRating, instrumentation, interiorDesign, qualityOfMaterials, logicOfControls, interiorRating, cargoStorage, acceleration, roadHolding, braking,
+                       performanceRating, shifting, steering]
+
+                records.append(new_record)
+
+        except Exception, e:
+            print e
+
+
+
+
+# print json
+
+
+# loop through articles and add them into a list
+
+# save articles to a dataframe
+records_df = pd.DataFrame(records, columns= ['brand', 'year', 'model','text_review', 'userRating', 'styleId', 'id', 'purchaseDate', 'pricePaid', 'odometerMiles',
+                                              'title', 'commentsCount', 'averageRating', ' rearSeats', 'frontSeats',
+                                              'comfortRating', 'gettingInOut', 'rideComfort', 'noiseAndVibration',
+                                              'authorName', 'climateControl', 'bluetooth', 'usbPorts', 'technologyRating',
+                                              'navigation', 'parkingAids', 'outwardVisibility', 'rainSnowTraction', 'safetyRating',
+                                              'activeSafety', 'headlights', 'safetyRating', 'instrumentation', 'interiorDesign',
+                                              'qualityOfMaterials', 'logicOfControls', 'interiorRating', 'cargoStorage', 'acceleration',
+                                              'roadHolding', 'braking', 'performanceRating', 'shifting', 'steering'])
+
+try:
+    print "started excel creation"
+    records_df.to_excel('new_t_subaru.xlsx')
+    print "excel created"
+except Exception,e:
+    print e
